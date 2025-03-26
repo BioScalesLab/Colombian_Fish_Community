bc929ed569febccfa96edb2f8af01919593fd894
Juan Pablo Quimbayo <quimbayojp@miami.edu>
2025-03-25 21:18:33 -0400

    Functions and packages

---
+++ Code/Packages.R
@@ -0,0 +1,22 @@
+# Loading Packages --------------------------
+
+if(!require(dplyr)){install.packages('dplyr'); library(dplyr)}
+if(!require(tibble)){install.packages('tibble'); library(tibble)}
+if(!require(ggplot2)){install.packages('ggplot2'); library(ggplot2)}
+if(!require(reshape)){install.packages('reshape'); library(reshape)}
+if(!require(ggpubr)){install.packages('ggpubr'); library(ggpubr)}
+if(!require(car)){install.packages('car'); library(car)}
+if(!require(plyr)){install.packages('plyr'); library(plyr)}
+if(!require(vegan)){install.packages('vegan'); library(vegan)}
+if(!require(ade4)){install.packages('ade4'); library(ade4)}
+if(!require(FD)){install.packages('FD'); library(FD)}
+if(!require(tidyr)){install.packages('tidyr'); library(tidyr)}
+if(!require(stringr)){install.packages('stringr'); library(stringr)}
+if(!require(viridis)){install.packages('viridis'); library(viridis)}
+if(!require(tidyverse)){install.packages('tidyverse'); library(tidyverse)}
+if(!require(stringr)){install.packages('stringr'); library(stringr)}
+if(!require(ggthemes)){install.packages('ggthemes'); library(ggthemes)}
+if(!require(lubridate)){install.packages('lubridate'); library(lubridate)}
+if(!require(reshape2)){install.packages('reshape2'); library(reshape2)}
+if(!require(gridExtra)){install.packages('gridExtra'); library(gridExtra)}
+
